2098f12ef940d9fe4df9f8f70bb94f5f7b52bc52
Julia Seeliger <julia@seeliger.cc>
2024-11-26 16:23:51 +0100

    Add files via upload

---
+++ sexd.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+#let ChatGPT write a Sex Dice for me 
+#things went on 
+
+import random 
+import os
+from time import sleep
+
+actions = ["Nudeln", "Kartoffeln", "Reis", "Polenta", "Kartoffelbrei", "Hirsetaler", "Pommes"]
+body_parts = ["Gemüse", "Gemüse und Tofu", "Pilzen und Gemüse", "Soß", "Pilzen und Tofu", "Salat"] 
+
+input("Was wollen Sie essen? ENTER Drücken um es herauszufinden!")
+
+def roll_sex_dice(): 
+	action = random.choice(actions) 
+	body_part = random.choice(body_parts) 
+	return f"{action} mit {body_part}" 
+
+# sleep for 2 seconds after having asked then clean screen
+sleep(2)
+os.system('clear')
+
+print("Sie wollen", roll_sex_dice(), "essen.")
+
+#clean at end for next use
+sleep(5)
+os.system('clear')
